bc33006d969f1a4f797abe47eb83b8e58c3e11c8
buonhobo <naturytropik@gmail.com>
2024-01-20 14:35:20 +0100

    added scripts to start the cluster with and without kind

---
+++ projects/asw-order-manager/start-cluster-kind.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#Start cluster
+kind create cluster --config kind-config.yaml
+
+#This makes the ingress-nginx work with KIND
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
+kubectl wait --namespace ingress-nginx \
+  --for=condition=ready pod \
+  --selector=app.kubernetes.io/component=controller \
+  --timeout=90s #This is just waiting for that "patch" to apply
+
+source start-cluster.sh
--- projects/asw-order-manager/start-cluster.sh
+++ projects/asw-order-manager/start-cluster.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
-kind create cluster --config kind-config.yaml
-kubectl create namespace ordermanager
-kubectl apply -f kubernetes -n ordermanager
+
+
+#Start Kafka using helm
+helm install kafka --set listeners.client.protocol=PLAINTEXT oci://registry-1.docker.io/bitnamicharts/kafka
+
+#Start postgres databases using helm
+helm install order-service-db \
+  --set global.postgresql.auth.postgresPassword=ordermanager \
+  oci://registry-1.docker.io/bitnamicharts/postgresql
+helm install product-service-db \
+  --set global.postgresql.auth.postgresPassword=ordermanager \
+  oci://registry-1.docker.io/bitnamicharts/postgresql
+helm install order-validation-service-db \
+  --set global.postgresql.auth.postgresPassword=ordermanager \
+  oci://registry-1.docker.io/bitnamicharts/postgresql
+
+#Start ordermanager
+kubectl apply -f kubernetes
